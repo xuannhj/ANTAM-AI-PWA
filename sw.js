95087d7bc242dbab418c60f70b00eb5796ed3d28
Xuan Nhi Nguyen Pham <xuannhi.nguyenpham@gmail.com>
2026-08-12 00:43:27 +0700

    feat: Completed PWA setup and navigation flow

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'antam-ai-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './mat-than.html',
+  './khien.html',
+  './tro-ly.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
